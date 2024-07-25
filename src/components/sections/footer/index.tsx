5e98955bc5c5598d062ec2c3e83f80f7d2db7e7c
nawesan12 <nahueltecnicomdp@gmail.com>
2024-07-25 00:55:50 -0300

    effect with gsap to the footer imagee

--- src/components/sections/footer/index.tsx
+++ src/components/sections/footer/index.tsx
@@ -1,11 +1,28 @@
+"use client";
+import { useEffect, useRef } from "react";
+import { gsap } from "gsap";
 import Image from "next/image";
 
 export default function Footer() {
+  const imageRef = useRef(null);
+
+  useEffect(() => {
+    // GSAP animation for the image
+    gsap.to(imageRef.current, {
+      scale: 1.1,
+      duration: 3,
+      yoyo: true,
+      repeat: -1,
+      ease: "easeInOut",
+    });
+  }, []);
+
   return (
     <footer className="mx-auto grid place-items-center">
       <h3 className="text-[14vw] font-bold leading-none flex flex-col max-w-min">
         <span className="flex items-center gap-6 justify-end">
           <Image
+            ref={imageRef}
             src="/footer-waves.webp"
             alt=""
             width={200}
