a54383c165a68ac993e07f65e780c6ef0a9d97a7
Nahuel Santillan <nahueltecnicomdp@gmail.com>
2024-07-25 15:33:18 -0300

    a

--- src/components/sections/footer/index.tsx
+++ src/components/sections/footer/index.tsx
@@ -3,21 +3,40 @@ import Image from "next/image";
 export default function Footer() {
   return (
     <footer className="mx-auto grid place-items-center">
-      <h3 className="text-[14vw] font-bold leading-none flex flex-col max-w-min">
-        <span className="flex items-center gap-6 justify-end">
+      <section>
+        <h3 className="text-[14vw] font-bold leading-none flex flex-col max-w-min">
+          <span className="flex items-center gap-6 justify-end">
+            <Image
+              src="/footer-waves.webp"
+              alt=""
+              width={200}
+              height={200}
+              className="w-[20vw] aspect-video relative bottom-4 right-4"
+            />
+            WEAR
+          </span>
+          <span className="text-black [text-shadow:-1px_-1px_0_white,_1px_-1px_0_white,_-1px_1px_0_white,_1px_1px_0_white]">
+            EVERYDAY
+          </span>
+        </h3>
+      </section>
+
+      <section className="border-t-2 mt-4 border-white w-full p-4 lg:grid-cols-6 ">
+        <section className="grid-cols-3 ">
+          <h2>basement.</h2>
+
           <Image
-            src="/footer-waves.webp"
+            src="/globe.png"
             alt=""
-            width={200}
-            height={200}
-            className="w-[20vw] aspect-video relative bottom-4 right-4"
+            width={100}
+            height={100}
           />
-          WEAR
-        </span>
-        <span className="text-black [text-shadow:-1px_-1px_0_white,_1px_-1px_0_white,_-1px_1px_0_white,_1px_1px_0_white]">
-          EVERYDAY
-        </span>
-      </h3>
+
+          &copy; basement.studio &reg; all rights reserved.
+        </section>
+        <section></section>
+      </section>
+
     </footer>
   );
 }
