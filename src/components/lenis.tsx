8a591ca765e0ebaef977bddb663175f6b228376d
nawesan12 <nahueltecnicomdp@gmail.com>
2024-07-25 00:47:46 -0300

    lenis smooth scrolling

---
+++ src/components/lenis.tsx
@@ -0,0 +1,32 @@
+"use client";
+import Lenis, { LenisOptions } from "@studio-freight/lenis";
+import { useEffect } from "react";
+
+export default function LenisController({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  useEffect(() => {
+    const lenis = new Lenis({
+      duration: 1.2,
+      easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)), // https://www.desmos.com/calculator/brs54l4xou
+      orientation: "vertical", // vertical, horizontal
+      gestureOrientation: "vertical", // vertical, horizontal, both
+      smoothWheel: true,
+      wheelMultiplier: 1,
+      touchMultiplier: 2,
+      infinite: false,
+    });
+
+    function raf(time: number) {
+      //@ts-ignore
+      lenis.raf(time);
+      requestAnimationFrame(raf);
+    }
+
+    requestAnimationFrame(raf);
+  }, []);
+
+  return <>{children}</>;
+}
