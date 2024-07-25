bc20a84abeba387ae1fdbad779943d454582e6e9
nawesan12 <nahueltecnicomdp@gmail.com>
2024-07-25 00:23:03 -0300

    mocked up footer

--- src/components/sections/footer/index.tsx
+++ src/components/sections/footer/index.tsx
@@ -1,7 +1,23 @@
+import Image from "next/image";
+
 export default function Footer() {
   return (
-    <>
-      <h3 className="text-[14vw] font-bold leading-none">WEAR EVERYDAY</h3>
-    </>
+    <footer className="mx-auto grid place-items-center">
+      <h3 className="text-[14vw] font-bold leading-none flex flex-col max-w-min">
+        <span className="flex items-center gap-6 justify-end">
+          <Image
+            src="/footer-waves.webp"
+            alt=""
+            width={200}
+            height={100}
+            className="max-h-[12vw]"
+          />
+          WEAR
+        </span>
+        <span className="text-black [text-shadow:-1px_-1px_0_white,_1px_-1px_0_white,_-1px_1px_0_white,_1px_1px_0_white]">
+          EVERYDAY
+        </span>
+      </h3>
+    </footer>
   );
 }
