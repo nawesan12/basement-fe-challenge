d622e1abcaa551778dcdf7aa62ec5fa8cdf75848
nawesan12 <nahueltecnicomdp@gmail.com>
2024-07-25 00:35:17 -0300

    fixed marquee and added favicon

--- src/components/ui/marquee.tsx
+++ src/components/ui/marquee.tsx
@@ -6,28 +6,49 @@ import Image from "next/image";
 export default function Marquee() {
   const marqueeRef = useRef(null);
   const containerRef = useRef(null);
+  const imageRef1 = useRef(null);
+  const imageRef2 = useRef(null);
 
   useEffect(() => {
+    // Levitation effect for the images
+    gsap.to(imageRef1.current, {
+      y: -10, // Move up by 10 pixels
+      repeat: -1, // Repeat infinitely
+      yoyo: true, // Reverse animation direction
+      ease: "power1.inOut", // Smooth easing
+      duration: 2, // Duration of each half of the animation
+    });
+
+    gsap.to(imageRef2.current, {
+      y: -10, // Move up by 10 pixels
+      repeat: -1, // Repeat infinitely
+      yoyo: true, // Reverse animation direction
+      ease: "power1.inOut", // Smooth easing
+      duration: 2, // Duration of each half of the animation
+      delay: 1, // Add a delay to desynchronize the animations
+    });
+
+    // Infinite marquee effect
     const marquee = marqueeRef.current;
     const container = containerRef.current;
     //@ts-ignore
-    const marqueeWidth = marquee?.scrollWidth;
+    const marqueeWidth = marquee.scrollWidth;
 
-    // Clone the marquee content
+    // Create a clone of the marquee content
     //@ts-ignore
-    const clone = marquee?.cloneNode(true);
+    const clone = marquee.cloneNode(true);
     //@ts-ignore
-    container?.appendChild(clone);
+    container.appendChild(clone);
 
-    gsap.set(clone, { position: "absolute", top: 0, left: marqueeWidth });
+    gsap.set([marquee, clone], { display: "inline-block" });
 
     gsap.to(container, {
       x: -marqueeWidth,
       duration: 10,
       ease: "linear",
       repeat: -1,
-      onRepeat: () => {
-        gsap.set(container, { x: 0 });
+      modifiers: {
+        x: (x) => `${parseFloat(x) % marqueeWidth}px`, // Ensure smooth looping
       },
     });
   }, []);
@@ -35,15 +56,15 @@ export default function Marquee() {
   return (
     <section className="relative max-w-screen p-4 border-t-2 border-b-2 border-white">
       <div
-        className="relative text-2xl font-bold overflow-hidden whitespace-nowrap"
+        className="relative text-2xl font-bold whitespace-nowrap"
         ref={containerRef}
       >
         <div ref={marqueeRef} className="inline-block">
           <span className="mx-2">
-            - A man can{`'`}t have enought basement. swag
+            - A man can{`'`}t have enough basement. swag
           </span>
           <span className="mx-2">
-            -- A man can{`'`}t have enought basement. swag
+            -- A man can{`'`}t have enough basement. swag
           </span>
         </div>
       </div>
@@ -51,14 +72,16 @@ export default function Marquee() {
       <Image
         src="/marquee-shape.svg"
         alt=""
-        className="absolute left-20"
+        ref={imageRef1}
+        className="absolute left-20 object-contain"
         width={100}
         height={100}
       />
       <Image
         src="/marquee-shape2.svg"
         alt=""
-        className="absolute right-24 bottom-8"
+        ref={imageRef2}
+        className="absolute right-24 bottom-8 object-contain"
         width={100}
         height={100}
       />
