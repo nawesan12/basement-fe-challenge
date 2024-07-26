8803ea0cab4b44b7adb8ecfcaa458134183b50c3
nawesan12 <nahueltecnicomdp@gmail.com>
2024-07-26 01:15:18 -0300

    fixed responsive

--- src/components/ui/cart.tsx
+++ src/components/ui/cart.tsx
@@ -6,10 +6,16 @@ export default function Cart() {
     <SheetContent className="bg-black p-0 w-screen lg:h-3/4 md:h-full lg:w-2/4 md:w-3/4 flex flex-col justify-between">
       <SheetHeader>
         <SheetTitle>
-          <h3 className="lg:text-7xl md:text-6xl text-[30vw] text-center leading-none px-2 text-white mt-20 lg:whitespace-nowrap">
-            {" "}
-            YOUR <span className="hidden lg:block md:block">&nbsp;</span>
+          <h3 className="hidden md:block lg:block lg:text-7xl md:text-6xl text-center leading-none px-2 text-white mt-20 whitespace-nowrap">
+            YOUR{" "}
             <span className="text-black [text-shadow:-1px_-1px_0_white,_1px_-1px_0_white,_-1px_1px_0_white,_1px_1px_0_white]">
+              CART
+            </span>
+          </h3>
+
+          <h3 className="block md:hidden lg:hidden text-[30vw] text-center leading-none px-2 text-white mt-20">
+            YOUR
+            <span className="block text-black [text-shadow:-1px_-1px_0_white,_1px_-1px_0_white,_-1px_1px_0_white,_1px_1px_0_white]">
               CART
             </span>
           </h3>
@@ -21,12 +27,14 @@ export default function Cart() {
       </section>
 
       <SheetFooter>
-        <section className="lg:border-t border-white w-full flex-col lg:flex-row text-center flex justify-between">
-          <div className="font-bold py-4 px-6 lg:px-8 text-3xl text-white whitespace-nowrap flex justify-between">
-            <span>TOTAL: </span> &nbsp; <span>$37,50</span>
+        <section className="lg:border-t border-white w-full flex-col lg:flex-row items-center text-center flex justify-between lg:border-b lg:border-l">
+          <div className="font-bold py-4 px-6 lg:px-8 text-3xl text-white whitespace-nowrap flex w-full justify-between lg:justify-start lg:space-x-4">
+            <span>TOTAL: </span>
+            <span>$37,50</span>
           </div>
+
           <hr className="w-11/12 pt-4 block mx-auto lg:hidden" />
-          <span className="text-black py px-6 lg:px-8 font-bold text-6xl md:text-7xl text-center lg:text-3xl [text-shadow:-1px_-1px_0_white,_1px_-1px_0_white,_-1px_1px_0_white,_1px_1px_0_white] lg:border-l border-l-white gap-4">
+          <span className="text-black py-4 px-6 lg:px-8 font-bold text-6xl md:text-7xl text-center lg:text-3xl [text-shadow:-1px_-1px_0_white,_1px_-1px_0_white,_-1px_1px_0_white,_1px_1px_0_white] lg:border-l border-l-white gap-4">
             CHECKOUT
           </span>
         </section>
--- src/components/ui/sheet.tsx
+++ src/components/ui/sheet.tsx
@@ -31,7 +31,7 @@ const SheetOverlay = React.forwardRef<
 SheetOverlay.displayName = SheetPrimitive.Overlay.displayName;
 
 const sheetVariants = cva(
-  "fixed z-50 sm:w-screen lg:w-2/4 w-3/4 gap-4 bg-background p-6 transition ease-in-out data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:duration-300 data-[state=open]:duration-500 max-h-screen h-screen",
+  "fixed z-50 border-white sm:w-screen lg:w-2/4 w-3/4 gap-4 bg-background p-6 transition ease-in-out data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:duration-300 data-[state=open]:duration-500 max-h-screen h-screen",
   {
     variants: {
       side: {
@@ -40,7 +40,7 @@ const sheetVariants = cva(
           "inset-x-0 bottom-0 border-t data-[state=closed]:slide-out-to-bottom data-[state=open]:slide-in-from-bottom",
         left: "inset-y-0 left-0 h-full w-3/4 border-r data-[state=closed]:slide-out-to-left data-[state=open]:slide-in-from-left",
         right:
-          "inset-y-0 right-0 max-h-min sm:min-w-screen md:w-3/4 lg:w-2/4 lg:border-l md:border-l lg:border-b data-[state=closed]:slide-out-to-right data-[state=open]:slide-in-from-right",
+          "inset-y-0 right-0 max-h-min sm:min-w-screen md:w-3/4 lg:w-2/4 lg:border-l md:border-l md:border-b lg:border-b data-[state=closed]:slide-out-to-right data-[state=open]:slide-in-from-right",
       },
     },
     defaultVariants: {
